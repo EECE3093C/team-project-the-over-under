ee8bc834d2ae77960c273b26123e7785abd83d66
dickenjd <123788731+dickenjd@users.noreply.github.com>
2023-02-24 12:47:19 -0500

    Updated selenium_Draftkings.py
    Added variables for each website we are web-scraping off of.

--- selenium_DraftKings.py
+++ selenium_DraftKings.py
@@ -3,14 +3,19 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-
+# Variables for each websites PL odds
+DraftKings = "https://sportsbook.draftkings.com/leagues/soccer/england---premier-league"
+Fanduel = "https://sportsbook.fanduel.com/soccer?tab=epl"
+Bet365 = "https://www.oh.bet365.com/?_h=XG4D8hz5aYaLFAgHTDdlYA%3D%3D#/AC/B1/C1/D1002/E76169570/G40/H^1/"
+BetMGM = "https://sports.oh.betmgm.com/en/sports/soccer-4/betting/england-14"
+PointsBet = "https://oh.pointsbet.com/sports/soccer/English-Premier-League/138839"
 
 # Open Draftkings, had to use an "Options" workaround online as it refused to stay open
 # using a regular PATH
 chrome_options = Options()
 chrome_options.add_experimental_option('detach', True)
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://sportsbook.draftkings.com/leagues/soccer/england---premier-league")
+driver.get(DraftKings)
 print(driver.title)
 ready = 0
 driver.maximize_window()
