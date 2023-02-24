b9b523321da0f1fe60c806356bb3cd35844776da
dickenjd <123788731+dickenjd@users.noreply.github.com>
2023-02-24 12:05:05 -0500

    Update selenium_Draftkings.py
    Removed unnecessary code and added extra comments

--- selenium_DraftKings.py
+++ selenium_DraftKings.py
@@ -6,19 +6,17 @@ from selenium.webdriver.common.by import By
 
 
 # Open Draftkings, had to use an "Options" workaround online as it refused to stay open
-# using a regular PATH
 chrome_options = Options()
 chrome_options.add_experimental_option('detach', True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://sportsbook.draftkings.com/leagues/soccer/england---premier-league")
+# Verification that we're on the correct page
 print(driver.title)
-ready = 0
-driver.maximize_window()
-spans = driver.find_elements("tag name", "div")
-divlist = []
 
+# Action of printing each bit of text on the page into individual lines
 webpage = driver.find_element("tag name", "html")
 print(webpage.text)
 
+# Action of creating a new text file and writing what the terminal prints inside
 Parsefile = open('DraftKingsPLOdds.txt', 'w')
 Parsefile.writelines(webpage.text)
