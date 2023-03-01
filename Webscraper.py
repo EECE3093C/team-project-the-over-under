864dcf36535a72c700adc0f07cbc21a046f4d1c2
unknown <blairbowen35@gmail.com>
2023-03-01 16:52:29 -0500

    remove comments

--- Webscraper.py
+++ Webscraper.py
@@ -35,57 +35,57 @@ for item in scrapedList:
         item = "-" + item[12:]
     Parsefile.write(item+"\n")
 
-# # Now we can run this on all 5 websites from one file
-# # Fanduel Scraping
-# driver.get(Fanduel)
-# time.sleep(10)
-# print(driver.title)
+# Now we can run this on all 5 websites from one file
+# Fanduel Scraping
+driver.get(Fanduel)
+time.sleep(10)
+print(driver.title)
 
-# webpage = driver.find_element("tag name", "html")
-# print(webpage.text)
+webpage = driver.find_element("tag name", "html")
+print(webpage.text)
 
-# Parsefile = open('PLOddsFanDuel.txt', 'w')
-# Parsefile.writelines("FANDUEL PREMIER LEAGUE MONEYLINE ODDS \n")
-# Parsefile.writelines(webpage.text)
-
-
-# # Barstool Scraping
-# driver.get(Barstool)
-# time.sleep(10)
-# print(driver.title)
-
-# webpage = driver.find_element("tag name", "html")
-# print(webpage.text)
-
-# Parsefile = open('PLOddsBarstool.txt', 'w')
-# Parsefile.writelines("BARSTOOL PREMIER LEAGUE MONEYLINE ODDS \n")
-# scrapedList = str(webpage.text.encode())[2:-1].split("\\n")
-# for item in scrapedList:
-#     Parsefile.write(item+"\n")
+Parsefile = open('PLOddsFanDuel.txt', 'w')
+Parsefile.writelines("FANDUEL PREMIER LEAGUE MONEYLINE ODDS \n")
+Parsefile.writelines(webpage.text)
 
 
-# # BetMGM Scraping
-# driver.get(BetMGM)
-# time.sleep(10)
-# print(driver.title)
+# Barstool Scraping
+driver.get(Barstool)
+time.sleep(10)
+print(driver.title)
 
-# webpage = driver.find_element("tag name", "html")
-# print(webpage.text)
+webpage = driver.find_element("tag name", "html")
+print(webpage.text)
 
-# Parsefile = open('PLOddsBetMGM.txt', 'w')
-# Parsefile.writelines("BETMGM PREMIER LEAGUE MONEYLINE ODDS \n")
-# Parsefile.writelines(webpage.text)
+Parsefile = open('PLOddsBarstool.txt', 'w')
+Parsefile.writelines("BARSTOOL PREMIER LEAGUE MONEYLINE ODDS \n")
+scrapedList = str(webpage.text.encode())[2:-1].split("\\n")
+for item in scrapedList:
+    Parsefile.write(item+"\n")
 
 
-# # PointsBet Scraping
-# driver.get(PointsBet)
-# time.sleep(10)
-# print(driver.title)
+# BetMGM Scraping
+driver.get(BetMGM)
+time.sleep(10)
+print(driver.title)
 
-# webpage = driver.find_element("tag name", "html")
-# print(webpage.text)
+webpage = driver.find_element("tag name", "html")
+print(webpage.text)
 
-# Parsefile = open('PLOddsPointsBet.txt', 'w')
-# Parsefile.writelines("POINTSBET PREMIER LEAGUE MONEYLINE ODDS \n")
-# Parsefile.writelines(webpage.text)
+Parsefile = open('PLOddsBetMGM.txt', 'w')
+Parsefile.writelines("BETMGM PREMIER LEAGUE MONEYLINE ODDS \n")
+Parsefile.writelines(webpage.text)
+
+
+# PointsBet Scraping
+driver.get(PointsBet)
+time.sleep(10)
+print(driver.title)
+
+webpage = driver.find_element("tag name", "html")
+print(webpage.text)
+
+Parsefile = open('PLOddsPointsBet.txt', 'w')
+Parsefile.writelines("POINTSBET PREMIER LEAGUE MONEYLINE ODDS \n")
+Parsefile.writelines(webpage.text)
 driver.close()
