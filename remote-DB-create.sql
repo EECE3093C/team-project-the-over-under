582656f631c37f89aa3609825784c4b2b31094ad
ciminibb <108684607+ciminibb@users.noreply.github.com>
2023-03-03 12:47:40 -0500

    Update remote-DB-create.sql
    #48 add PostgreSQL scripts for baseline population

--- remote-DB-create.sql
+++ remote-DB-create.sql
@@ -1,3 +1,4 @@
+-- Create tables per data model as of 2/23.
 CREATE TABLE competitions (
     competition VARCHAR(30) NOT NULL, -- 30 characters seems to be an upper bound for name length
     PRIMARY KEY (competition)
@@ -24,3 +25,36 @@ CREATE TABLE moneylines (
     FOREIGN KEY (contestant) REFERENCES contestants(contestant),
     FOREIGN KEY (sportsbook) REFERENCES sportsbooks(sportsbook)
 );
+
+-- Populate tables with first set of "permanent" data.
+INSERT INTO competitions
+VALUES ('Premier League');
+INSERT INTO contestants(contestant, competition)
+VALUES
+    ('Arsenal', 'Premier League'),
+    ('Man City', 'Premier League'),
+    ('Man United', 'Premier League'),
+    ('Tottenham', 'Premier League'),
+    ('Newcastle', 'Premier League'),
+    ('Fulham', 'Premier League'),
+    ('Brighton', 'Premier League'),
+    ('Liverpool', 'Premier League'),
+    ('Brentford', 'Premier League'),
+    ('Chelsea', 'Premier League'),
+    ('Aston Villa', 'Premier League'),
+    ('Crystal Palace', 'Premier League'),
+    ('Nottm Forest', 'Premier League'),
+    ('Leicester City', 'Premier League'),
+    ('Wolves', 'Premier League'),
+    ('Everton', 'Premier League'),
+    ('Bournemouth', 'Premier League'),
+    ('West Ham', 'Premier League'),
+    ('Leeds United', 'Premier League'),
+    ('Southampton', 'Premier League');
+INSERT INTO sportsbooks
+VALUES
+    ('FanDuel', 'https://www.fanduel.com/'),
+    ('BetMGM', 'https://sports.oh.betmgm.com/en/sports'),
+    ('DraftKings', 'https://www.draftkings.com/?wpsrc=Organic%20Search&wpaffn=Google&wpkw=https%3A%2F%2Fwww.draftkings.com%2F'),
+    ('PointsBet', 'https://oh.pointsbet.com/'),
+    ('Barstool', 'https://www.barstoolsportsbook.com/')
