3a5132392473c84bfde78a7135a0ecac0998fb90
unknown <blairbowen35@gmail.com>
2023-03-04 14:18:15 -0500

    added gitignore and correct the parser names

---
+++ src/SportsbookParsers/FanDuelParser.py
@@ -0,0 +1,21 @@
+import re
+
+def FanDuelParser():
+    file = open("data/PLOddsFanDuel.txt")
+    lines = file.readlines()
+    index = 0
+    current = 0
+    naughtindex = []
+    for i in lines:
+        if current + 2 < index and re.search("^([+]|[-])", i) != None:
+            current = index
+            naughtindex.append(index)
+        index += 1
+
+    for i in naughtindex:
+        print("{0} vs {1}\n{0} Wins: {2}, Draw: {3}, {1} Wins: {4}\n\n".format(
+            lines[i-2][:-1] if lines[i-1][:-1] != '0' else lines[i-4][:-1],
+            lines[i-1][:-1] if lines[i-1][:-1] != '0' else lines[i-2][:-1], 
+            lines[i][:-1], 
+            lines[i+1][:-1], 
+            lines[i+2][:-1]))
