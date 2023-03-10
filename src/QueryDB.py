76f58e1b51143b83689ca6840e4525f457629e1c
Ben Cimini <ciminibb@mail.uc.edu>
2023-03-09 21:21:25 -0500

    Established test connection to remote db #47

--- src/QueryDB.py
+++ src/QueryDB.py
@@ -1,1 +1,22 @@
-print("Testing...")
+import bitdotio
+import pandas as pd
+
+# Connect with bit.io API key credentials.
+b = bitdotio.bitdotio("v2_3zxXD_wujECdYdhKwuU7nzWFG8M57")
+
+# Define queries as below.
+insert_test_row = """
+    INSERT INTO matchups(home, away, competition)
+    VALUES
+        ('Bournemouth', 'Liverpool', 'Premier League')
+    """
+
+delete_test_row = """
+    DELETE FROM matchups
+    WHERE home = 'Bournemouth'
+    """
+
+# Execute queries with cursor.
+with b.get_connection("ciminibb/mainline") as conn:
+    cursor = conn.cursor()
+    cursor.execute(insert_test_row)
